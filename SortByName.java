7b8446ab77531bf49893d5a51f7579bd0a416955
davey561 <davey561@gmail.com>
2018-12-02 13:50:51 -0500

    yo

--- SortByName.java
+++ SortByName.java
@@ -4,6 +4,6 @@
 import java.util.Comparator;
  public class SortByName implements Comparator<Student>{
   public int compare (Student a, Student b){
-    return a.getName().compareTo(b.getName());
+    return a.getName().compareTo(b.getName()); //compares names of students
   }
  }
